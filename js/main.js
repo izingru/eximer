8eb21b833e5bfafc7bc032dc230fdda07345eb5e
izingru <146376594+izingru@users.noreply.github.com>
2023-10-30 10:41:22 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -569,7 +569,6 @@
       });
     }
 
-
     if ($('.js-popup_btn__appointment').length) {
       agPopup ('.js-popup_btn__appointment', '.js-popup-form__appointment', '.js-popup-form_btn__close__appointment');
     }
@@ -622,9 +621,7 @@
       });
 
       $(window).on('scroll', function () {
-
         $('.js-faq_box:visible').each(function () {
-
           if ($(this).offset().top - 100 < $(window).scrollTop()) {
             $('.js-faq-tab_link').removeClass('ag-faq-tab_link__active');
             $('.js-faq-tab_link[href=#' + $(this).attr('id') + ']').addClass('ag-faq-tab_link__active');
@@ -636,87 +633,106 @@
     /* /tab scroll */
 
     /* parallax */
-    $.fn.parallax = function(options) {
+    if ($('.js-ag-primary_parallax').length) {
 
-      var windowHeight = $(window).height();
+      $.fn.parallax = function(options) {
 
-      // Establish default settings
-      var settings = $.extend({
-        isPosBG         : true,
-        xPosBG          : '50%',
-        yPosBG          : 0,
-        speedBG         : 0.15,
+        var windowHeight = $(window).height();
 
-        isPos           : true,
-        yPosBottom      : 0,
-        speedPosBottom  : 0,
-        direction       : 1
-      }, options);
+        // Establish default settings
+        var settings = $.extend({
+          isPosBG         : true,
+          xPosBG          : '50%',
+          yPosBG          : 0,
+          speedBG         : 0.15,
 
-      // Iterate over each object in collection
-      return this.each( function() {
+          isPos           : true,
+          yPosBottom      : 0,
+          speedPosBottom  : 0,
+          direction       : 1
+        }, options);
 
-        // Save a reference to the element
-        var $this = $(this);
+        // Iterate over each object in collection
+        return this.each( function() {
 
-        // Set up Scroll Handler
-        $(document).scroll(function(){
+          // Save a reference to the element
+          var $this = $(this);
 
-          var scrollTop = $(window).scrollTop();
-          var offset = $this.offset().top;
-          var height = $this.outerHeight();
+          // Set up Scroll Handler
+          $(document).scroll(function(){
 
-          // Check if above or below viewport
-          if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
-            return;
-          }
+            var scrollTop = $(window).scrollTop();
+            var offset = $this.offset().top;
+            var height = $this.outerHeight();
 
-          var yPositionBG = Math.round((offset - scrollTop) * settings.speedBG) + parseInt(settings.yPosBG),
-            yPositionBoxBottom = Math.round((offset - scrollTop) * settings.speedPosBottom) + parseInt(settings.yPosBottom);
+            // Check if above or below viewport
+            if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+              return;
+            }
 
-          // Apply the Y Background Position to Set the Parallax Effect
-          if(settings.isPosBG) {
-            $this.css({'background-position': settings.xPosBG + yPositionBG + 'px'});
-          }else if(settings.isPos) {
-            $this.css({'bottom': settings.direction * yPositionBoxBottom + 'px'});
-          } else {
-            $this.css({'background-position': settings.xPosBG + yPositionBG + 'px', 'bottom': settings.direction * yPositionBoxBottom + 'px'});
-          }
+            var yPositionBG = Math.round((offset - scrollTop) * settings.speedBG) + parseInt(settings.yPosBG),
+              yPositionBoxBottom = Math.round((offset - scrollTop) * settings.speedPosBottom) + parseInt(settings.yPosBottom);
 
-        });
-      });
-    };
+            // Apply the Y Background Position to Set the Parallax Effect
+            if(settings.isPosBG) {
+              $this.css({'background-position': settings.xPosBG + yPositionBG + 'px'});
+            }else if(settings.isPos) {
+              $this.css({'bottom': settings.direction * yPositionBoxBottom + 'px'});
+            } else {
+              $this.css({'background-position': settings.xPosBG + yPositionBG + 'px', 'bottom': settings.direction * yPositionBoxBottom + 'px'});
+            }
 
-    window.mobileAndTabletCheck = function() {
-      var check = false;
-      (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
-      return check;
-    };
-
-    var agDisplayWidth = $(window).width();
-
-    $(window).resize(function () {
-      if (agDisplayWidth > '750') {
-        if(!window.mobileAndTabletCheck()) {
-          $('.js-primary_parallax__home').parallax({
-            xPosBG: '100%',
-            yPosBG: 0,
-            speedBG: -0.15,
-
-            isPos: false
           });
+        });
+      };
 
-          /*$('.js-primary_parallax__phone').parallax({
-            isPos: false,
-            yPosBG: 0,
-            speedBG: -0.1
-          });*/
+      window.mobileAndTabletCheck = function() {
+        var check = false;
+        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+        return check;
+      };
+
+      var agDisplayWidth = $(window).width();
+
+      $(window).resize(function () {
+        if (agDisplayWidth > '750') {
+          if(!window.mobileAndTabletCheck()) {
+            $('.js-ag-primary_parallax').parallax({
+              xPosBG: '100%',
+              yPosBG: 0,
+              speedBG: -0.4,
+
+              isPos: false
+            });
+          }
         }
-      }
 
-    }).resize();
+      }).resize();
+    }
     /* /parallax */
 
+
+    /* scroll animation */
+    if ($('.js-anim_box').length) {
+      function agScrollAnimation() {
+        var agAnimItem = $('.js-anim_box'),
+          agWindowHeight = $(window).outerHeight(),
+          agOffsetCoefficient= .9;
+
+        agAnimItem.each(function (i, elem) {
+          if ($(window).scrollTop() >= $(elem).offset().top - agWindowHeight * agOffsetCoefficient - 100) {
+            $(elem).find('.js-anim_content').addClass('js-ag-anim__active').removeClass('js-ag-anim__inactive');
+          } else {
+            $(elem).find('.js-anim_content').removeClass('js-ag-anim__active').addClass('js-ag-anim__inactive');
+          }
+        })
+      }
+
+      $(window).on('scroll load', function () {
+        agScrollAnimation();
+      });
+    }
+    /* /scroll animation */
 
 
     $('#popup-appointment-phone').mask('+380(999) 999-99-99');
